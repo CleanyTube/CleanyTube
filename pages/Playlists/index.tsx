7ead6adbf4061bcc4ef405fc94b439c65fe480df
NathanFirmo <nathanssfirmo@gmail.com>
2023-03-28 21:34:03 -0300

    feat: add fab on playlist page

--- pages/Playlists/index.tsx
+++ pages/Playlists/index.tsx
@@ -1,11 +1,36 @@
 import { ScrollView } from 'native-base'
+import { Fab, Icon, Box, Center, NativeBaseProvider, View } from 'native-base'
+import React from 'react'
+import { AntDesign } from '@expo/vector-icons'
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  fab: {
+    position: 'absolute',
+    bottom: 25,
+    right: 25,
+  },
+})
 
 export const Playlists = () => {
   return (
-    <ScrollView
-      _dark={{
-        backgroundColor: 'black',
-      }}
-    ></ScrollView>
+    <View style={{ flex: 1 }}>
+      <ScrollView
+        _dark={{
+          backgroundColor: 'black',
+        }}
+      ></ScrollView>
+
+      <View>
+        <Fab
+          renderInPortal={false}
+          shadow={2}
+          backgroundColor="red.400"
+          size="sm"
+          icon={<Icon color="white" as={AntDesign} name="plus" size="sm" />}
+          style={styles.fab}
+        />
+      </View>
+    </View>
   )
 }
